bff5e0ae44b55bc0378b2568f432832fe88ea581
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-05-12 08:04:11 -0700

    Update Variables and Constants.rs

--- Chapter 3 - Common Programing Concepts/Variables and Constants.rs
+++ Chapter 3 - Common Programing Concepts/Variables and Constants.rs
@@ -2,7 +2,7 @@
 
 //But there are some differences between "immutable variables" and "constants":
 //1) You are not allowed to use the "mut" keyword with constants. Constants are always immutable.
-//2) You declare a constant by using the "const" keyword instead of the "let" keyword, and the type of the value must be annotated.
+//2) You declare a constant by using the "const" keyword instead of the "let" keyword, and the type of the value must be annotated. In other words, you must specify what is the type of the constant that you are declaring.
 //3) Constants can be declared in any scope inside your code.
 //4) The last and, perhaps, the most important difference is that constants may be set only to a constant expression, not the result of a function call or any other value that could only be computed at runtime.
 

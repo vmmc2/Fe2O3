cbc85e14e34a74a8365c0ab58a4b00abd45fc773
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-05-12 08:01:26 -0700

    Update Variables and Mutability.rs

--- Chapter 3 - Common Programing Concepts/Variables and Mutability.rs
+++ Chapter 3 - Common Programing Concepts/Variables and Mutability.rs
@@ -9,7 +9,7 @@ fn main(){
   let x = 5;
   println!("The value of x is: {}", x);
   x = 6;                                //This code will not compile because I am trying to change an immutable variable...
-  println("The value of x is: {}", x); 
+  println!("The value of x is: {}", x); 
 }
 
 // But mutability can be very useful. Variables are immutable only by default; 
@@ -19,13 +19,24 @@ fn main(){
 
 fn main(){
   let mut x = 5;
-  println("The value of x is: {}", x);
+  println!("The value of x is: {}", x);
   x = 23;
-  println("The value of x now is: {}", x);
+  println!("The value of x now is: {}", x);
 }
 
 // There are multiple trade-offs to consider in addition to the prevention of bugs. For example, in cases where you’re using large data
 // structures, mutating an instance in place may be faster than copying and returning newly allocated instances. With smaller data 
 // structures, creating new instances and writing in a more functional programming style may be easier to think through, so lower 
 // performance might be a worthwhile penalty for gaining that clarity.
+
+//We also are allowed to declare a variable (be it mutable or immutable) and not assign an initial value to it...
+//For example, we can do the following:
+fn main(){
+  let mux a;
+  a = 17;
+  println!("the value of a is: {}.", a);
+  a = 234;
+  println!("now, the value of a is: {}.", a);
+}
+
 

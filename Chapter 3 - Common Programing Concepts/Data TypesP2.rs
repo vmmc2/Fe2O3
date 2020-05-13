2023c9a1b20633b1c5628c1fab1b19c9ed508046
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-05-13 12:08:12 -0700

    Update Data TypesP2.rs

--- Chapter 3 - Common Programing Concepts/Data TypesP2.rs
+++ Chapter 3 - Common Programing Concepts/Data TypesP2.rs
@@ -1,8 +1,8 @@
-//Rust is a statically-typed language. This means that it must know the type of all variables at compile-time
+//Rust is a statically-typed language. This means that it must know the type of all variables at compile-time.
 //The Rust Compiler, as well as the GHC, can usually infer what type we want to use based on its value and how we use it.
 
 //Scalar Type:
-// A scalar type represents a single value. Rust has four primary scalar types: booleans, characters, integers, floating-point
+// A scalar type represents a single value. Rust has four primary scalar types: booleans, characters, integers, floating-point.
 
 //4) Characters
 /*
@@ -13,7 +13,7 @@
 just a ASCII value.
 - By a lot more, we mean: Accented letters, chinese, japanese and korean characters. Oh.. and emojis too.
 - To be more specific, when we are dealing with Unicode Scalar Values we are dealing with values that are in the ranges below:
-  I) U+0000 to U+D7FF
+  I) U+0000 to U+D7FF.
   II) U+E000 to U+10FFFF, inclusive.
 - Also, we have a type annotation for characters too in Rust: we use the keyword 'char'.
 */

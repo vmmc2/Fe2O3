519a27741c8835dd1993157b474c29308ac8812b
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-05-18 09:08:00 -0700

    Update Celsius-Farenheit Conversor.rs

--- Chapter 3 - Common Programing Concepts/Celsius-Farenheit Conversor.rs
+++ Chapter 3 - Common Programing Concepts/Celsius-Farenheit Conversor.rs
@@ -10,4 +10,10 @@ fn farenheit_to_celsius(temp: f32){
 fn main(){
     let number: f32 = 36.5;
     celsius_to_farenheit(number);
+    fp_division();
+}
+
+fn fp_division(){
+    let result: f32 = (32 as f32)/6.0; //This is another way to convert from an integer number to a floating-point number (of 32-bits).
+    println!("The value of the variable result is: {}", result);
 }

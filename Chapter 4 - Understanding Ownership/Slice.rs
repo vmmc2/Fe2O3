503a5bbb535aaed4e1aeedd62e11ba5f12321772
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-05-25 15:25:32 -0700

    Update Slice.rs

--- Chapter 4 - Understanding Ownership/Slice.rs
+++ Chapter 4 - Understanding Ownership/Slice.rs
@@ -45,6 +45,41 @@ fn main(){
   //Os String Slices funcionam exatamente como os ranges. A extremidade esquerda é inclusiva enquanto que a extremidade direita é exclusiva.
   let hello = &s[0..5];
   let world = &s[6..11];
+  /*
+  - This is similar to taking a reference to the whole String but with the extra [0..5] bit. Rather than a reference to the entire
+  String, it’s a reference to a portion of the String.
+  - We can create slices using a range within brackets by specifying [starting_index..ending_index], where starting_index is the first
+  position in the slice and ending_index is one more than the last position in the slice.
+  */
+}
+
+fn slices_examples(){
+  /*
+  - With Rust’s .. range syntax, if you want to start at the first index (zero), you can drop the value before the two periods. 
+  */
+  let s = String::from("hello");
+  let slice = &s[0..2];
+  let slice = &s[..2];
+  
+  /*
+  - By the same token, if your slice includes the last byte of the String, you can drop the trailing number.
+  */
+  let s = String::from("hello caraio");
+  let len = s.len();
+  let slice = &s[3..len];
+  let slice = &s[3..];
+  
+  /*
+  - You can also drop both values to take a slice of the entire string. So these are equal:
+  */
+  let s = String::from("Tensa Zangetsu!!");
+  let len = s.len();
+  let slice = &s[0..len];
+  let slice = &s[..];
+  
+  
+  
   
 }
+
 
